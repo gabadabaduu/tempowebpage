0cedcc28808536717219ef123b0f6fb75f3145c6
gabadabaduu <luigigabriela68@gmail.com>
2025-02-17 13:10:48 -0500

    navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PrivacypolicyComponent } from './privacypolicy/privacypolicy.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent} from './contact/contact.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'privacypolicy', component: PrivacypolicyComponent}
+  {path: 'privacypolicy', component: PrivacypolicyComponent},
+  {path: 'navbar', component: NavbarComponent},
+  {path: 'contact', component: ContactComponent}
 ];
