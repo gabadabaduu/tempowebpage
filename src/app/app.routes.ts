4dc1e4999a3f520c2c31e87436a993d3912fc95a
gabadabaduu <luigigabriela68@gmail.com>
2025-02-16 23:06:02 -0500

    privacy policy, navbar and home components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PrivacypolicyComponent } from './privacypolicy/privacypolicy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'privacypolicy', component: PrivacypolicyComponent}
+];
